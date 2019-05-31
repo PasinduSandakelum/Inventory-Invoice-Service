261df8a1bf3a6fa0c8b87d11d45352cfd93caa3e
waruna95 <waruna.wijesundara@my.sliit.lk>
2019-05-31 12:56:14 +0530

    RewardPoint Modal initialize

---
+++ invoice-service/src/main/java/com/virtusa/inventory/invoiceservice/repository/RewardPointRepository.java
@@ -0,0 +1,11 @@
+package com.virtusa.inventory.invoiceservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.virtusa.inventory.invoiceservice.model.RewardPoint;
+
+public interface RewardPointRepository extends JpaRepository<RewardPoint, Integer>{
+	
+
+}
